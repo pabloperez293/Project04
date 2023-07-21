d39ef2cc99a2d28a953b9f132d6813c78fb67441
pabloperez293 <paperezpablo@gmail.com>
2023-07-21 16:42:25 -0300

    preventDefault

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
 let contador = 0;
 
-boton.addEventListener("click", function(){
+boton.addEventListener("click", function(evt){
+  evt.preventDefault();
   if(contador == 0){
     enlaces.className = ("enlaces dos");
     contador = 1 ;
