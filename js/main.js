cf774fd45cb7d1305c5f234e9be4e11209ff4910
pabloperez293 <paperezpablo@gmail.com>
2023-07-21 16:41:31 -0300

    out #

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
 
-let enlaces{
-  
-}
+boton.addEventListener("click", function(){
+  if(contador == 0){
+    enlaces.className = ("enlaces dos");
+    contador = 1 ;
+  }else{
+    enlaces.classList.remove("dos");
+    enlaces.className = ("enlaces uno")
+    contador = 0 ;
+  }
+})
